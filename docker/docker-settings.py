db4221dbe1250dc9ee64b488b44328c28eb3e740
Camila Reyes <cami_jara@hotmail.es>
2018-12-20 19:27:52 +0100

    dockerfile with waitress

--- docker/docker-settings.py
+++ docker/docker-settings.py
@@ -27,16 +27,16 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'http://10.5.0.1:8000'
+BASEURL = 'http://localhost:$PORT'
 
 APIS = {
-    'authentication': 'http://10.5.0.1:8000',
-    'base': 'http://10.5.0.1:8000',
-    'booth': 'http://10.5.0.1:8000',
-    'census': 'http://10.5.0.1:8000',
-    'mixnet': 'http://10.5.0.1:8000',
-    'postproc': 'http://10.5.0.1:8000',
-    'store': 'http://10.5.0.1:8000',
-    'visualizer': 'http://10.5.0.1:8000',
-    'voting': 'http://10.5.0.1:8000',
+    'authentication': 'http://localhost:$PORT',
+    'base': 'http://localhost:$PORT',
+    'booth': 'http://localhost:$PORT',
+    'census': 'http://localhost:$PORT',
+    'mixnet': 'http://localhost:$PORT',
+    'postproc': 'http://localhost:$PORT',
+    'store': 'http://localhost:$PORT',
+    'visualizer': 'http://localhost:$PORT',
+    'voting': 'http://localhost:$PORT',
 }
